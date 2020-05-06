9c0f0388852b53ed61d156b1fe312b95fc05abe5
Dan Edie <daedie2@gmail.com>
2020-05-06 08:18:58 -0500

    Edits to Homework

--- EmployeeSQL/employee_schema.sql
+++ EmployeeSQL/employee_schema.sql
@@ -8,8 +8,8 @@ DROP TABLE IF EXISTS titles;
 
 -- Create the tables
 CREATE TABLE titles(
-	title_id VARCHAR(5),
-	title VARCHAR(255),
+	title_id VARCHAR(5) NOT NULL,
+	title VARCHAR(255) NOT NULL,
 	PRIMARY KEY (title_id)
 );
 
@@ -17,9 +17,9 @@ CREATE TABLE employees(
 	emp_no INT NOT NULL,
 	emp_title_id VARCHAR(5) NOT NULL,
 	birth_date DATE NOT NULL,
-	first_name VARCHAR(255),
-	last_name VARCHAR(255),
-	sex VARCHAR(4),
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
 	hire_date DATE NOT NULL,
 	PRIMARY KEY (emp_no),	
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
@@ -39,15 +39,15 @@ CREATE TABLE dept_manager(
 );
 
 CREATE TABLE dept_emp(
-	emp_no INT,
-	dept_no VARCHAR(4),
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE salaries(
-	emp_no INT,
-	salary INT,
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
 	PRIMARY KEY (emp_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
